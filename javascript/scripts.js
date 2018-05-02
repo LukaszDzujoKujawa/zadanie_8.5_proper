ff3c27c9b6a4ee0377a1953d0113d4ed71cd1ef0
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-05-02 22:48:32 +0200

    modify javascript 2

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -6,4 +6,4 @@ triangleArea = a * h / 2;
 
 var a = 4;
 var h = 7;
-console.log('Triangle field with height h: ' + h + ' and base a: ' + a + ' is equal to: ' + triangleArea );
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);	
