716e507bb92a1b8e4f693b8f94dc3193f6690633
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-05-02 22:45:32 +0200

    modify javascript

--- JS/scripts.js
+++
@@ -1,4 +0,0 @@
-var triangleArea = a * h / 2;
-var a = 12;
-var h = 5;
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
---
+++ javascript/scripts.js
@@ -0,0 +1,9 @@
+var a,
+	h,
+	triangleArea;
+
+triangleArea = a * h / 2;
+
+var a = 4;
+var h = 7;
+console.log('Triangle field with height h: ' + h + ' and base a: ' + a + ' is equal to: ' + triangleArea );
