cb197d86d7759694058c5ae0b7968d94afa07791
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-05-03 21:02:49 +0200

    javascript add var

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -2,8 +2,9 @@ var a,
 	h,
 	triangleArea;
 
+a = 4;
+h = 7;
+
 triangleArea = a * h / 2;
 
-var a = 4;
-var h = 7;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);	
